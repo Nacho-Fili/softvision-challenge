6a30ce4e583eebc38b41839782ffa7978eb9c0fe
Ignacio Filipovskis <n.filipovskis@gmail.com>
2021-09-02 17:59:35 -0300

    Variable de entorno para axios agregada

--- src/steps/api.ts
+++ src/steps/api.ts
@@ -2,8 +2,10 @@ import axios from "axios";
 
 import {ApiService} from "./types";
 
+const ENDPOINT = process.env.REACT_APP_ENDPOINT;
+
 const api: ApiService = {
-  getSteps: () => axios.get("http://localhost:3000/steps").then(({data}) => data),
+  getSteps: () => axios.get(`${ENDPOINT}/steps`).then(({data}) => data),
 };
 
 export default api;
